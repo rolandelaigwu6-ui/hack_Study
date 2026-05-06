9bf5805b20ad96b6a843674a40ddfa930326d5b7
rolandelaigwu6-ui <rolandelaigwu6@gmail.com>
2026-05-06 03:10:30 +0100

    my ascii-loadbanner

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func Loadbanner(filename string) (map[rune][]string, error) {
 	banner := make(map[rune][]string)
 	currentrune := rune(32)
 	for i := 0; i < len(lines); i+=9 {
-		if i+8 <= len(lines) {
+		if i+8 > len(lines) {
 			return nil, fmt.Errorf("Invalid: at rune %d", currentrune)
 		}
 		characterBlock := lines[i+1 : i+9]
@@ -52,3 +52,23 @@ func Loadbanner(filename string) (map[rune][]string, error) {
 
 }
 
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . <text> <banner_file>")
+		return
+	}
+
+	input := os.Args[1]
+	filename := os.Args[2]
+
+	banner, err := Loadbanner(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Loaded successfully")
+	fmt.Println("Input:", input)
+	fmt.Println("Total characters loaded:", len(banner))
+}
