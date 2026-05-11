bdfd04b199374a8dab86e2b77db5ef0e31ea296e
rolandelaigwu6-ui <rolandelaigwu6@gmail.com>
2026-05-11 09:33:15 +0000

    my ascii-art project

---
+++ ascii/loadbanner.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+func loadbanner( filaname string) (map[rune][]string, error) {
+	if len(filename) == 0 {
+		return nil, fmt.Errorf("filename is empty")
+	}
+	file,err := os.Open(filename) 
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %v", err)
+	}
+	defer file.Close()
+	var line []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line = append(line, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file %v", err)
+	}
+	if len(line) != 855 {
+		return nil, fmt.Errorf("unexpected number of line got %d want 855", len(line))
+	}
+	banner := make(map[rune][]string)
+	currentrune := rune(32)
+	for i := 0; i < len(line); i += 8 {
+		if i+8 > len(line) {
+			return nil, fmt.Errorf("unsupported  rune %v at index %d", currentrune, i)
+		}
+		charblock := line[i+1 : i+9]
+		banner[currentrune] = charblock
+		currentrune++
+	}
+	return banner, nil
+
+	
+}
